c908cabf9dea7e573658caac9e77b45d570f7e91
Suraj Rathi <suraj.rathi00@gmail.com>
2021-06-17 10:57:06 +0530

    visualize metrics pairwise

--- visualize_two_param_scatterplot.py
+++ visualize_two_param_scatterplot.py
@@ -1,22 +1,36 @@
 #! /usr/bin/env python
 import itertools
 
-import matplotlib.pyplot
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 FILES = ['./videos/ppt_test.mp4', './videos/wb_test.mp4']
 
 
 def main():
-    FILE = FILES[1]
+    sns.set()
+    fig, axes = plt.subplots(2, 3, figsize=(30, 20))
+    axes = axes.reshape(-1)
+    for ax, (aa, bb) in zip(axes, itertools.combinations(('perc_bg', 'perc_ch', 'perc_ow', 'perc_oc'), 2), ):
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
+        ax.axis('equal')
+        ax.set_xlabel(aa)
+        ax.set_ylabel(bb)
+
+    plot_video(FILES[0], axes)
+    plot_video(FILES[1], axes)
+
+    fig.show()
+
+
+def plot_video(FILE, axes):
     VIDEO_NAME = FILE[:FILE.rfind('.')] if (index := FILE.rfind('/')) == -1 else FILE[index + 1:FILE.rfind('.')]
     KEY_FRAMES_FILENAME = f"./videos/{VIDEO_NAME}.txt"
     METRICS_DATA_FILENAME = f"./videos/{VIDEO_NAME}.csv"
-
     metrics = pd.read_csv(METRICS_DATA_FILENAME, header=None).to_numpy()
-
     keyframes = []
     with open(KEY_FRAMES_FILENAME) as f:
         for line in f:
@@ -31,24 +45,9 @@ def main():
     perc_ow = metrics[2] / 10
     perc_oc = metrics[3]
 
-    import seaborn as sns
-    import matplotlib.pyplot as plt
-    fig, axes = plt.subplots(2, 3, figsize=(30, 20))
-    axes = axes.reshape(-1)
-    for ax in axes:
-        ax.set_xlim(0, 1)
-        ax.set_ylim(0, 1)
-        ax.axis('equal')
-
-    for (a, b), (aa, bb), ax in zip(itertools.combinations((perc_bg, perc_ch, perc_ow, perc_oc), 2),
-                                    itertools.combinations(('perc_bg', 'perc_ch', 'perc_ow', 'perc_oc'), 2),
-                                    axes):
+    for (a, b), ax in zip(itertools.combinations((perc_bg, perc_ch, perc_ow, perc_oc), 2), axes):
         sns.scatterplot(ax=ax, x=a, y=b, hue=np.in1d(np.arange(metrics.shape[1]), keyframes))
         ax.legend([], [], frameon=False)
-        ax.set_xlabel(aa)
-        ax.set_ylabel(bb)
-
-    matplotlib.pyplot.show()
 
 
 if __name__ == '__main__':
